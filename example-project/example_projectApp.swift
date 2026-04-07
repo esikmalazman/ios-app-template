1c39ce4b1b0ef156582b59f92026c29a36c0c67c
esikmalazman <opikmalazman@gmail.com>
2026-04-07 10:07:33 +0800

    set sweetpad for cursor

---
+++ example-project/example_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  example_projectApp.swift
+//  example-project
+//
+//  Created by esikmalazman on 07/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct example_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
